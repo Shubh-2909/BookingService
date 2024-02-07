dab92553a0f1da1017f02e08b0640ba095de2dec
Shubh Shubhanjal <shubhshubhanjal96@gmail.com>
2024-02-07 16:57:13 +0530

    Added rabit mq config

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -2,26 +2,40 @@ const {BookingService} = require('../services/index');
 const {StatusCodes} = require('http-status-codes')
 
 const bookingService = new BookingService();
+const {createChannel , publishMessage} = require('../utils/messageQueue')
+const {REMINDER_BINDING_KEY} = require('../config/serverConfig')
 
-const create = async (req,res) => {
-    try {
-        const response = await bookingService.createBooking(req.body);
-        return res.status(StatusCodes.OK).json({
-            message : 'Successfully completed booking',
-            success: true,
-            err: {},
-            data: response
+class BookingController{
+    constructor(){
+        
+    }
+    async sendMessageToQueue(req, res){
+        const channel = await createChannel();
+        const data = {message : "Success"}
+        publishMessage(channel , REMINDER_BINDING_KEY , JSON.stringify(data) );
+        return res.status(200).json({
+            message : "successfully published the message"
         })
-    } catch (error) {
-        return res.status(error.statusCode).json({
-            message : error.message,
-            success: false,
-            err: error.explanation,
-            data: {}
-        })
+    }
+    async  create(req,res) {
+        try {
+            const response = await bookingService.createBooking(req.body);
+            return res.status(StatusCodes.OK).json({
+                message : 'Successfully completed booking',
+                success: true,
+                err: {},
+                data: response
+            })
+        } catch (error) {
+            return res.status(error.statusCode).json({
+                message : error.message,
+                success: false,
+                err: error.explanation,
+                data: {}
+            })
+        }
     }
 }
 
-module.exports={
-    create
-}
+
+module.exports=BookingController
